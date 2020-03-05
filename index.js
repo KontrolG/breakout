5c72012c657584f36631d8b3638cbadcf9e656fc
German Solano <kontrolg1239@gmail.com>
2020-03-05 05:44:15 -0400

    Logica de juego

--- index.js
+++ index.js
@@ -12,74 +12,187 @@ const playBoom = () => {
   boomAudio.play();
 }
 
+const showMessage = (message, marginTop = 0, colorString = "#fff") => {
+  canvasContext.fillStyle = colorString;
+  const { width: textWidth } = canvasContext.measureText(message);
+  canvasContext.fillText(
+    message,
+    canvas.width / 2 - textWidth / 2,
+    canvas.height / 2 + marginTop
+  );
+}
+
+const showOverlay = () => {
+  canvasContext.fillStyle = "rgba(51, 51, 51, 0.7";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const gameOver = () => {
+  state.currentState = "gameOver";
+  showOverlay();
+  showMessage("You lost!", -13, "crimson");
+  showMessage("Click anywhere to restart.", 16);
+  cancelAnimationFrame(render);
+}
+
+const gameFinished = () => {
+  state.currentState = "gameFinished";
+  showOverlay();
+  showMessage("You won!", -13, "forestgreen");
+  showMessage("Click anywhere to restart.", 16);
+  cancelAnimationFrame(render);
+};
+
+const gamePaused = () => {
+    showOverlay();
+    showMessage("Game Paused", -13, "orange");
+    showMessage("Click anywhere to continue.", 16);
+    cancelAnimationFrame(render);
+}
+
 const render = () => {
-  canvasContext.fillStyle = "rgba(51, 51, 51, 0.5";
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = "rgba(51, 51, 51, 1";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
   canvasContext.fillStyle = "rgba(255, 255, 255, 1";
-  canvasContext.font = "26px sans-serif";
-  canvasContext.fillText(`Loses: ${state.loses}`, 15, 45);
-
+  canvasContext.fillText(`Lives: ${state.lives}`, 15, 45);
+  canvasContext.fillText(`Bricks: ${state.bricks.length}`, canvas.width - 120, 45);
 
   state.playerPad.draw(canvasContext);
   state.bouncingBall.draw(canvasContext);
   for (const brick of state.bricks) {
-    state.bouncingBall.checkCollision(brick);
+    state.bouncingBall.didCollideWith(brick);
     brick.draw();
   }
 
 
-  state.bouncingBall.checkCollision(state.playerPad);
+
+  state.bouncingBall.didCollideWith(state.playerPad);
   state.bouncingBall.update(canvas.width, canvas.height);
+
+  if (state.lives === 0) {
+    gameOver();
+    return;
+  } else if (!state.bricks.length) {
+    gameFinished();
+    return;
+  } else if (state.currentState === "paused") {
+    gamePaused();
+    return;
+  }
 
   requestAnimationFrame(render);
 }
 
 const loadGame = () => {
-  state.loses = 0;
+  canvasContext.font = "26px 'Lato'";
+  canvasContext.fillStyle = "rgba(51, 51, 51, 1";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  state.currentState = "standBy";
+  showOverlay();
+  showMessage("Click anywhere to restart.", -13);
 
   state.playerPad = new PlayerPad(
     canvas.width / 2 - 150 / 2,
     canvas.height - 25,
-    "magenta",
+    "blueviolet",
     150,
     25
   );
 
-  state.bouncingBall = new bouncingBall(
-    canvas.width - 15,
-    (canvas.height / 2),
-    "magenta",
-    15,
-    -5,
-    0
-  );
+  /* initializateGame(); */
+
+  // Left Side test
+  // state.bouncingBall = new bouncingBall(
+  //   15,
+  //   canvas.height / 2 - 15,
+  //   "magenta",
+  //   15,
+  //   5,
+  //   0
+  // );
+
+  // Right Side test
+  // state.bouncingBall = new bouncingBall(
+  //   canvas.width - 15,
+  //   canvas.height / 2 - 5,
+  //   "magenta",
+  //   15,
+  //   -5,
+  //   0
+  // );
+
+  // Top side (for the right) test
+  // state.bouncingBall = new bouncingBall(15, 15, "blueviolet", 15, 10, 10);
+
+  // Top side (for the left) test
+  // state.bouncingBall = new bouncingBall(
+  //   canvas.width - (190 + 165),
+  //   15,
+  //   "magenta",
+  //   15,
+  //   0,
+  //   5
+  // );
+
+  // Bottom side (for the right) test
+  // state.bouncingBall = new bouncingBall(
+  //   canvas.width - 190,
+  //   canvas.height - 100,
+  //   "magenta",
+  //   15,
+  //   0,
+  //   -5
+  // );
+
+  // Bottom side (for the left) test
+  // state.bouncingBall = new bouncingBall(
+  //   canvas.width - (190 + 165),
+  //   canvas.height - 100,
+  //   "magenta",
+  //   15,
+  //   0,
+  //   -5
+  // );
+}
+
+const initializateGame = () => {
+  state.lives = 3;
+  state.currentState = "playing";
+
+  state.bouncingBall = new bouncingBall(15, 15, "blueviolet", 15, 5, 5);
 
   state.bricks = [
     new Brick(
       canvas.width / 2 + 160,
-      canvas.height / 2 - 20 / 2,
-      "olive",
+      canvas.height / 2 - 10 / 2,
+      "skyblue",
       150,
-      20
+      10
     ),
     new Brick(
       canvas.width / 2 - 50 / 2,
       canvas.height / 2 - 25 / 2,
-      "olive",
+      "skyblue",
       150,
       25
     ),
     new Brick(
       canvas.width / 2 - 200,
       canvas.height / 2 - 30 / 2,
-      "olive",
+      "skyblue",
       150,
       30
     )
   ];
 
   render();
+}
+
+const continueGame = () => {
+  state.currentState = "playing";
+  requestAnimationFrame(render);
 }
 
 const handleControls = e => {
@@ -91,7 +204,7 @@ const handleControls = e => {
   else if(keyCode === 39)
     state.playerPad.move(30, rightLimit);
   else if(keyCode === 27) 
-    debugger;
+    state.currentState = "paused";
 }
 
 const handleMouse = e => {
@@ -100,6 +213,14 @@ const handleMouse = e => {
   state.playerPad.move(distance, rightLimit);
 }
 
+const handleClick = () => {
+  if (state.currentState !== "playing") {
+    if (state.currentState === "paused") continueGame();
+    else initializateGame();
+  }
+}
+
 window.addEventListener("keydown", handleControls);
 window.addEventListener("mousemove", handleMouse);
+canvas.addEventListener("click", handleClick);
 window.addEventListener("load", loadGame);
--- models.js
+++ models.js
@@ -62,13 +62,14 @@ class bouncingBall extends Circle {
   }
 
   revertVelocity(axis) {
-    /* playBoom(); */
-    this[`velocity${axis}`] = -(this[`velocity${axis}`]);
+    playBoom();
+    this[`velocity${axis}`] = -this[`velocity${axis}`];
   }
 
   updatePosition(axis, axisLimit) {
     const newPosition = this[`position${axis}`] + this[`velocity${axis}`];
-    if (newPosition + this.radius >= axisLimit || newPosition <= this.radius) this.revertVelocity(axis);
+    if (newPosition + this.radius >= axisLimit || newPosition <= this.radius)
+      this.revertVelocity(axis);
     this[`position${axis}`] = newPosition;
   }
 
@@ -76,74 +77,113 @@ class bouncingBall extends Circle {
     this.updatePosition("X", rightLimit);
     this.updatePosition("Y", bottomLimit);
     if (this.positionY + this.radius >= canvas.height) {
-      state.loses++;
-      this.positionX = 1024 / 2;
-      this.positionY = 489 / 2;
+      state.lives--;
+      this.positionX = 20 /* 1024 / 2 */;
+      this.positionY = 20 /* 489 / 2 */;
     }
   }
 
-  checkCollision(otherShape) {
-    
-    const ballRightEdge = this.positionX + this.radius;
-    const ballBottomEdge = this.positionY + this.radius;
-    const ballTopEdge = this.positionY - this.radius;
-    const ballLeftEdge = this.positionX - this.radius;
-    
-    const otherShapeRightEdge = otherShape.positionX + otherShape.width;
-    const otherShapeBottomEdge = otherShape.positionY + otherShape.height;
-    
-    /* When the ball touched the top of the rectangle. */
-    const touchTop =
-      (ballRightEdge >= otherShape.positionX &&
-      ballLeftEdge <= otherShapeRightEdge) &&
-      (ballBottomEdge >= otherShape.positionY) &&
-      this.positionY <= otherShape.positionY &&
-      this.velocityY >= 0;
+  // checkCollision(otherShape) {
 
-    /* When the ball touched the bottom of the rectangle. */
-    const touchBottom =
-      ballRightEdge >= otherShape.positionX &&
-      ballLeftEdge <= otherShapeRightEdge &&
+  //   const ballRightEdge = this.positionX + this.radius;
+  //   const ballBottomEdge = this.positionY + this.radius;
+  //   const ballTopEdge = this.positionY - this.radius;
+  //   const ballLeftEdge = this.positionX - this.radius;
 
-      ballTopEdge <= otherShapeBottomEdge &&
-      this.positionY >= otherShapeBottomEdge &&
-      this.velocityY <= 0;
+  //   const otherShapeRightEdge = otherShape.positionX + otherShape.width;
+  //   const otherShapeBottomEdge = otherShape.positionY + otherShape.height;
 
+  //   /* When the ball touched the top of the rectangle. */
+  //   const touchTop =
+  //     ballRightEdge >= (otherShape.positionX) &&
+  //     ballLeftEdge <= (otherShapeRightEdge) &&
+  //     ballBottomEdge < otherShape.positionY &&
+  //     this.positionY >= otherShape.positionY &&
+  //     this.velocityY >= 0;
 
-      /* When the ball touch one side of the rectangle. */
-    /* Right side */
-    const touchRight =
-      this.positionX <= otherShapeRightEdge &&
-      ballTopEdge >= otherShape.positionY &&
-      ballRightEdge > otherShapeRightEdge &&
-      this.positionY <= otherShapeBottomEdge &&
-      this.velocityX <= 0;
-      
-    /* Left side */
-    const touchLeft =
-      /* Siempre que sobrepase el lado izq sera true */
-      ballRightEdge >= otherShape.positionX &&
-      /* Cuando sobrepase el borde superior */
-      ballTopEdge >= otherShape.positionY &&
-      /*  */
-      ballLeftEdge < otherShape.positionX &&
-      this.positionY <= otherShapeBottomEdge &&
-      this.velocityX >= 0;
+  //   /* When the ball touched the bottom of the rectangle. */
+  //   const touchBottom =
+  //     ballRightEdge >= (otherShape.positionX) &&
+  //     ballLeftEdge <= (otherShapeRightEdge) &&
 
-      if (ballLeftEdge <= otherShapeRightEdge) {
-        debugger;
-      }
+  //     ballTopEdge < otherShapeBottomEdge &&
+  //     this.positionY >= otherShapeBottomEdge &&
+  //     this.velocityY <= 0;
 
-      if (!(otherShape instanceof PlayerPad) && touchTop || touchBottom || touchRight || touchLeft) {
-        // debugger;
-        otherShape.hitted();
-      }
+  //     /* When the ball touch one side of the rectangle. */
+  //   /* Right side */
+  //   const touchRight =
+  //   /* Done */
+  //     ballLeftEdge <= otherShapeRightEdge &&
 
-    if (touchTop || touchBottom) {
+  //     // ballTopEdge >= otherShape.positionY &&
+  //     ballBottomEdge >= otherShape.positionY &&
+
+  //     ballRightEdge > otherShapeRightEdge &&
+  //     ballTopEdge <= otherShapeBottomEdge &&
+  //     this.velocityX <= 0;
+
+  //   /* Left side */
+  //   const touchLeft =
+  //     /* Siempre que sobrepase el lado izq sera true */
+  //     ballRightEdge >= otherShape.positionX &&
+  //     /* Cuando sobrepase el borde superior */
+  //     // ballTopEdge >= otherShape.positionY &&
+  //     ballBottomEdge >= otherShape.positionY &&
+  //     /*  */
+  //     ballLeftEdge < otherShape.positionX &&
+  //     ballTopEdge <= otherShapeBottomEdge &&
+  //     this.velocityX >= 0;
+
+  //     /* if (ballRightEdge >= otherShape.positionX) {
+  //       debugger;
+  //     } */
+
+  //     if (!(otherShape instanceof PlayerPad) && (touchTop || touchBottom || touchRight || touchLeft)) {
+  //       // debugger;
+  //       otherShape.hitted();
+  //     }
+
+  //   if (touchTop || touchBottom) {
+  //     this.revertVelocity("Y");
+  //   } else if (touchRight || touchLeft) {
+  //     this.revertVelocity("X");
+  //   }
+  // }
+
+  didCollideWith(otherShape) {
+    const circunference = this.radius - (Math.PI / 2);
+    if (
+      this.positionX - circunference <
+        otherShape.positionX + otherShape.width &&
+      this.positionX + circunference > otherShape.positionX &&
+      this.positionY - circunference <
+        otherShape.positionY + otherShape.height &&
+      this.positionY + circunference > otherShape.positionY
+    ) this.changeDirection(otherShape);
+  }
+
+  changeDirection(otherShape) {
+    const circunference = this.radius - Math.PI / 2;
+    const secondAndSixth = (this.positionX - circunference >= otherShape.positionX &&
+    this.positionX - circunference <= otherShape.positionX + otherShape.width);
+
+    const fourthAndEight =
+      this.positionY + circunference >= otherShape.positionY &&
+      this.positionY + circunference <=
+        otherShape.positionY + otherShape.height;
+
+    if (secondAndSixth) {
       this.revertVelocity("Y");
-    }
-    else if (touchRight || touchLeft) {
+    } else if (fourthAndEight) {
       this.revertVelocity("X");
+    } else {
+      this.revertVelocity("Y");
+      this.revertVelocity("X");
+    }
+
+    if (otherShape instanceof Brick) {
+      otherShape.hitted();      
     }
   }
 }
@@ -154,8 +194,47 @@ class Brick extends Rectangle {
   }
 
   hitted() {
-    /* const index = state.bricks.indexOf(this);
-    state.bricks.splice(index, 1); */
-    this.colorString = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    this.colorString = `hsl(0, 0%, 30%)`;
+    const index = state.bricks.indexOf(this);
+    state.bricks.splice(index, 1);
   }
 }
+
+/* Factory */
+class BricksFactory {
+  constructor(processingBrickFunction) {
+    this.processingBrickFunction = processingBrickFunction;
+    this.brickClass(Brick);
+    // añadir una callback funcion para aplicar transformaciones a los ladrillos?
+  }
+
+  createBrick() {
+    // aplicar transformacion a las opciones
+    const options = this.processingBrickFunction(options);
+    return new this.brickClass(options);
+  }
+}
+/* 
+
+
+function proccesBricks() {
+  // establecer parametros del contenedor de los bloques.
+  // inicializar si no estan definidos.
+  this.cols = y;
+  this.rows = x;
+  this.width
+  this.height
+  this.originX = (canvas.width - this.width) / 2
+  this.originY = (canvas.width - this.width) / 2
+
+  bricksWidth, bricksHeight, bricksMargin;
+
+  // desde donde se creara el nuevo bloque.
+  actualPositionX, actualPositionY
+
+
+  // opciones del nuevo brick para que sea añadido a la lista y se renderize de acuerdo a la fabrica.
+  return options;
+}
+
+*/
